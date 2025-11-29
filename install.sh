8b4b232e67b025726744f9f96c97d1eb3c07bdb7
0CrazyLove <axelvasquez582@gmail.com>
2025-11-29 17:18:32 -0500

    eat(icons): add heart and illogical-impulse SVG icons to local share directory

--- install.sh
+++ install.sh
@@ -515,6 +515,7 @@ install_file() {
 }
 
 mkdir -p "$HOME/.config"
+mkdir -p "$HOME/.local" 
 
 print_info "Instalando configuraciones..."
 
@@ -530,7 +531,7 @@ install_config "$DOTFILES_DIR/.config/quickshell" "$HOME/.config/quickshell" "Qu
 install_config "$DOTFILES_DIR/.config/illogical-impulse" "$HOME/.config/illogical-impulse" "Illogical Impulse (Quickshell design)"
 install_config "$DOTFILES_DIR/wal" "$HOME/.config/wal" "Wal (Color schemes)"
 install_config "$DOTFILES_DIR/.config/xdg-desktop-portal" "$HOME/.config/xdg-desktop-portal" "XDG Desktop Portal (KDE)"
-
+install_config "$DOTFILES_DIR/.local/share" "$HOME/.local/share" ".local/share (iconos y recursos)"
 install_file "$DOTFILES_DIR/.config/starship.toml" "$HOME/.config/starship.toml" "Starship"
 
 # NUEVO: Inicializar submódulo shapes después de copiar quickshell config
@@ -600,6 +601,7 @@ check_config "$HOME/.config/illogical-impulse" "Illogical Impulse"
 check_config "$HOME/.config/starship.toml" "Starship"
 check_config "$HOME/.config/wal" "Wal"
 check_config "$HOME/.config/xdg-desktop-portal" "XDG Desktop Portal"
+check_config "$HOME/.local/share" ".local/share"
 check_config "/usr/local/bin/rm" "Script rm protector"
 
 # Verificar instalación de quickshell
